261c2d2ebad8f212600b51c6e7f0c59da7b5a4fd
JoaoSilva <jplimadev@gmail.com>
2026-01-20 20:04:48 -0300

    Initial Commit

---
+++ DayMode/DayModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayModeApp.swift
+//  DayMode
+//
+//  Created by Joao Paulo Lima Silva on 20/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DayModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
